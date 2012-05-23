235b09cb6adcc5c3efbece082ad455a13a337e7a
Christoffer Alvenstrand <christoffer@rbd.nu>
2012-05-23 13:06:58 +0200

    added myplaylist.php

---
+++ myplaylists.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<?php
+		include_once ('includes/head.php');
+	?>
+</head>
+<body>
+
+	<div id="container">
+		<?php
+            include_once ('includes/header.php');
+        ?>
+    
+        <div class="main" role="main">
+            
+        
+        </div>
+    
+        <?php
+            include_once ('includes/footer.php');
+        ?>
+	</div>
+</body>
+</html>
